20ef4de16fbe85e5af552b7d3ca52193fa4541af
unclebay <unclebigbay@gmail.com>
2021-06-07 00:56:44 +0100

    LOGGING

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -8,7 +8,8 @@ exports.getPlayers = async (req, res) => {
     const allPlayers = await client.query("SELECT * FROM developers.players");
     res.json(allPlayers);
   } catch (error) {
-    console.log("error 101", error);
-    res.status(400).json(`error from the db ${error}`);
+    res
+      .status(400)
+      .json(`error from the db ${error} ${process.env.INSTANCE_SCHEMA}`);
   }
 };
